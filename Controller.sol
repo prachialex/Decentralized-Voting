d9e9a55cdc68a574e428cee456cc9c765c6430b7
prachialex <prachialex@gmail.com>
2021-06-07 17:50:46 +0530

    initial commit

--- Controller.sol
+++ Controller.sol
@@ -7,11 +7,23 @@ contract Controller is Ownable {
     
     IStorage public store;
     
+     address private controller;
+    
+    event SetController(address indexed oldOwner, address indexed newOwner);
+    
+      constructor() {
+        controller = msg.sender; 
+        emit SetController(address(0), owner);
+    }
     modifier initialized () {
-        require( store != IStorage(address(0)), "Controller: Storage not initialised" );
+        require( store != IStorage(address(0)), "Controller: Storage not initialized" );
         _;
     }
-    
+   
+   modifier isValidVoter(uint _age)
+   {
+       require(_age>=18);
+   }
     function setStorage(IStorage _storage) onlyOwner external {
         require(_storage != IStorage(address(0)), "Controller: Storage 0");
         store = _storage;
@@ -19,12 +31,16 @@ contract Controller is Ownable {
     
     function setNewController(address _newController) onlyOwner public {
         store.setController(_newController);
-    }
     
-    function registerVoter(string memory _name, uint8 _age) initialized public {
-        store.regsiterVoter(_name, _age, msg.sender);
+     function registerVoter(string memory _name, uint8 _age) initialized isValidVoter public returns(bool) {
+        store.registerVoter(_name, _age, msg.sender);
+        return true;
     }
-        function updateAge(uint8 _newAge) initialised public {
+    function registerCandidate(string memory _name) initialized public {
+        store.registerCandidate(_name);
+        
+    }
+        function updateAge(uint8 _newAge) initialized public {
          store.updateAge(_newAge, msg.sender);
     }
 }
